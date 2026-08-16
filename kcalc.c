a512acac5ac92a9ceaae9a6e5868d72f1319dd86
SingleDog <a5457789889409@gmail.com>
2026-08-16 20:57:01 +0800

    feat: implemented tokenization

--- kcalc.c
+++ kcalc.c
@@ -1,3 +1,7 @@
+#include "asm-generic/errno.h"
+#include "linux/gfp_types.h"
+#include "linux/printk.h"
+#include "linux/slab.h"
 #include <linux/init.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
@@ -6,23 +10,33 @@
 #include <linux/moduleparam.h>
 #include <linux/stat.h>
 #include <linux/types.h>
+#include <linux/string.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("AMOGUS");
 MODULE_DESCRIPTION("A calculator that runs directly in the kernel.");
 
-static long a = 0, b = 0;
+static char *expr = "+";
 
-static char *op = "+";
+MODULE_PARM_DESC(expr, "Expression");
+module_param(expr, charp, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
-MODULE_PARM_DESC(a, "a");
-module_param(a, long, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+typedef enum {
+	TOKEN_OPERATOR,
+	TOKEN_OPERAND,
+	TOKEN_PAREN,
+	TOKEN_UNARY_MINUS,
+	TOKEN_UNARY_PLUS
+} token_t;
 
-MODULE_PARM_DESC(b, "b");
-module_param(b, long, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+typedef struct _TOKEN {
+	int num;
+	char op;
+	token_t type;
+} Token;
 
-MODULE_PARM_DESC(op, "op");
-module_param(op, charp, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+int tokenize(const char *, const int, int *, int *, Token **);
+void print_tokens_debug(const Token *, const int);
 
 static inline int calc(char op, long a, long b)
 {
@@ -30,17 +44,17 @@ static inline int calc(char op, long a, long b)
 	switch (op) {
 	case '+': {
 		if (check_add_overflow(a, b, &result))
-			pr_warn("Result overflowed.");
+			goto err_overflow;
 		pr_info("Result = %ld.", result);
 	} break;
 	case '-': {
 		if (check_sub_overflow(a, b, &result))
-			pr_warn("Result overflowed.");
+			goto err_overflow;
 		pr_info("Result = %ld", result);
 	} break;
 	case '*': {
 		if (check_mul_overflow(a, b, &result))
-			pr_warn("Result overflowed.");
+			goto err_overflow;
 		pr_info("Result = %ld", result);
 	} break;
 	case '/':
@@ -60,17 +74,17 @@ static inline int calc(char op, long a, long b)
 			return -EINVAL;
 		}
 		{
-			result = 1;
-			int i;
+			int i = 0;
 			bool overflowed = false;
 
 			// NOTE: 0^0 will be 1
 
-			for (i = 0; i < b; ++i)
+			result = 1;
+			for (; i < b; ++i)
 				overflowed |=
 					check_mul_overflow(result, a, &result);
 			if (overflowed)
-				pr_err("Result overflowed.");
+				goto err_overflow;
 			pr_info("Result = %ld.", result);
 		}
 		break;
@@ -78,29 +92,30 @@ static inline int calc(char op, long a, long b)
 		return -EINVAL;
 	}
 	return 0;
+err_overflow:
+	pr_info("Result overflowed.");
+	return -EOVERFLOW;
 }
 
 static int __init kcalc_init(void)
 {
-	int i = 0;
 	int rc = 0;
+	int expr_len = 0;
+	int token_len = 0, paren_count = 0;
+	Token *token_arr = NULL;
 
 	pr_info("kcalc is loaded successfully.");
-	pr_debug("(a, b) = (%ld, %ld).", a, b);
+	pr_debug("Expression = %s", expr);
+	expr_len = strlen(expr);
 
-	while (op[i] != '\0')
-		i++;
-
-	pr_debug("oplen = %d.", i);
-	if (i > 1 || i == 0)
-		goto err_inval;
-
-	pr_debug("Calculator mode %s.", op);
-
-	rc = calc(op[0], a, b);
+	rc = tokenize(expr, expr_len, &token_len, &paren_count, &token_arr);
+	pr_info("(token_len, paren_count) = (%d, %d)", token_len, paren_count);
+	print_tokens_debug(token_arr, token_len);
+	if (rc != 0)
+		goto err_tokenize;
+err_tokenize:
+	kfree(token_arr);
 	return rc;
-err_inval:
-	return -EINVAL;
 }
 
 static void __exit kcalc_exit(void)
@@ -110,3 +125,109 @@ static void __exit kcalc_exit(void)
 
 module_init(kcalc_init);
 module_exit(kcalc_exit);
+
+void print_tokens_debug(const Token *token_arr, const int token_len)
+{
+	int i = 0;
+	Token t;
+
+	pr_debug("----\n");
+	for (; i < token_len; ++i) {
+		t = token_arr[i];
+		switch (t.type) {
+		case TOKEN_OPERAND:
+			pr_debug("Token #%d: (type, val) = (operand, %d)", i,
+				 t.num);
+			break;
+		case TOKEN_OPERATOR:
+			pr_debug("Token #%d: (type, val) = (opeator, '%c')", i,
+				 t.op);
+			break;
+		case TOKEN_PAREN:
+			pr_debug(
+				"Token #%d: (type, val) = (paraenthesis, '%c')",
+				i, t.op);
+			break;
+		case TOKEN_UNARY_MINUS:
+			pr_debug("Token #%d: (type, val) = (unary_minus, '%c')",
+				 i, t.op);
+			break;
+		case TOKEN_UNARY_PLUS:
+			pr_debug("Token #%d: (type, val) = (unary_plus, '%c')",
+				 i, t.op);
+			break;
+		}
+	}
+	pr_debug("----\n");
+}
+
+int tokenize(const char *expr, const int expr_len, int *token_len,
+	     int *paren_count, Token **result)
+{
+	int expr_p = 0, tokens_p = 0;
+	int curr_num = 0;
+	int state = 0;
+	int parens = 0;
+	int rc = 0;
+	Token *tokens = (Token *)kmalloc(sizeof(Token) * expr_len, GFP_KERNEL);
+
+	if (!tokens) {
+		rc = -1;
+		goto err_alloc;
+	}
+
+	for (; expr_p < expr_len; ++expr_p) {
+		char ch = expr[expr_p];
+		if ('0' <= ch && ch <= '9') {
+			curr_num = curr_num * 10 + ch - '0';
+			state = 1;
+			continue;
+		}
+		if (state) {
+			tokens[tokens_p].type = TOKEN_OPERAND;
+			tokens[tokens_p].num = curr_num;
+			tokens[tokens_p].op = '\0';
+			curr_num = 0;
+			state = 0;
+			tokens_p++;
+		}
+		if (ch == ' ') {
+			continue;
+		} else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' ||
+			   ch == '%' || ch == '^') {
+			tokens[tokens_p].type = TOKEN_OPERATOR;
+			tokens[tokens_p].op = ch;
+			tokens_p++;
+		} else if (ch == '(' || ch == ')') {
+			tokens[tokens_p].type = TOKEN_PAREN;
+			tokens[tokens_p].op = ch;
+			parens++;
+			tokens_p++;
+		} else {
+			rc = -EINVAL;
+			goto err_inval;
+		}
+	}
+	if (state) {
+		tokens[tokens_p].type = TOKEN_OPERAND;
+		tokens[tokens_p].num = curr_num;
+		tokens_p++;
+	}
+
+	// If parens is odd then the parenthese must not be closed properly
+	if (parens % 2 != 0) {
+		rc = -EINVAL;
+		goto err_inval;
+	}
+
+	*token_len = tokens_p;
+	*paren_count = parens;
+	*result = tokens;
+	pr_debug("(token_len, paren_count) = (%d, %d)", tokens_p, parens);
+	return 0;
+
+err_inval:
+err_alloc:
+	kfree(tokens);
+	return rc;
+}
